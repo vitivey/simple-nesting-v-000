47091734a9a6b4c470d430c118ddd38cb68b7d1f
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-11 14:17:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -104,8 +104,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-     changing_alan[:alan_kay][:known_for]=alans_new_info
+    #  alans_new_info = "GUI"
+    #  changing_alan[:alan_kay][:known_for]=alans_new_info
 
 end
 
