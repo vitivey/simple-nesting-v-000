c3bc5df8f1fe21e01aaf39d7c5a3f33b2e5009d5
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-11 14:16:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -80,6 +83,7 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
     
 end
 
@@ -101,7 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     changing_alan[:alan_kay][:known_for]=alans_new_info
      
 end
 
